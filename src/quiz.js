0670b9141e1eed9de2f6955e07ce50c996cf431e
Nacho Monzón <ignaciomonzona@gmail.com>
2025-03-25 19:15:11 +0100

    Proyect complete

--- src/quiz.js
+++ src/quiz.js
@@ -1,43 +1,68 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
+
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+
+  // Day 2. filterQuestionsByDifficulty() method
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty !== "number" || difficulty < 1 || difficulty > 3) {
+      return;
     }
 
-    // 2. getQuestion()
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex];
-    }
-    
-    // 3. moveToNextQuestion()
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
-    }
+    this.questions = this.questions.filter(
+      (question) => question.difficulty === difficulty
+    );
+  }
 
-    // 4. shuffleQuestions()
-    shuffleQuestions() {
-        for (let i = this.questions.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-        }
-    }
+  // Day 2. averageDifficulty() method
+  averageDifficulty() {
+    if (this.questions.length === 0) return 0;
 
-    // 5. checkAnswer(answer)
-    checkAnswer(answer) {
-        const currentQuestion = this.getQuestion();
-        if (currentQuestion.answer === answer) {
-            this.correctAnswers++;
-        }
-    }
-
-    // 6. hasEnded()
-    hasEnded() {
-        return this.currentQuestionIndex >= this.questions.length;
-    }
+    const totalDifficulty = this.questions.reduce(
+      (sum, question) => sum + question.difficulty,
+      0
+    );
+    return totalDifficulty / this.questions.length;
+  }
 }
